6a254d447979b32ef094c70af36d7965672b76bf
unknown <jiawei.gong@shadowcreator.com>
2022-11-16 11:40:54 +0800

    controller-fw

---
+++ sku4-controller-test-k102/_build/aw_common.d
@@ -0,0 +1,6 @@
+..\..\..\_build\aw_common.o: ..\vibration\aw8624\aw_common.c
+..\..\..\_build\aw_common.o: ..\vibration\aw8624\aw_common.h
+..\..\..\_build\aw_common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\..\..\_build\aw_common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+..\..\..\_build\aw_common.o: ..\vibration\aw8624\aw8624.h
+..\..\..\_build\aw_common.o: ..\vibration\aw8624\aw8624_debug_attr.h
---
+++ sku4-controller-test-k102/_build/lsm6dsr_reg.d
@@ -0,0 +1,4 @@
+..\..\..\_build\lsm6dsr_reg.o: ..\imu\lsm6dsr\lsm6dsr_reg.c
+..\..\..\_build\lsm6dsr_reg.o: ..\imu\lsm6dsr\lsm6dsr_reg.h
+..\..\..\_build\lsm6dsr_reg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\..\..\_build\lsm6dsr_reg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
